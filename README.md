cb953e846933f9fc36ceac6e65e5f9e414d0b2e1
Tim Kelly <austimkelly@users.noreply.github.com>
2022-11-01 06:10:12 -0700

    adding first basic react native app with native navigation controller. (#1)
    * update readme
    
    * first project add with some basic navigation
    
    * Add in a placeholder screen for showing the gauge graph/chart
    
    * remove misnamed file that was breaking fresh build

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # riverdata-react-native
 River Data application written in React Native
+
+# Running the Project
+
+To run your project, navigate to the directory and run one of the following npm commands.
+
+- `$ cd riverdata`
+- `$ npm run android`
+- `$ npm run ios`
+- `$ npm run web`
---
+++ riverdata/App.js
@@ -0,0 +1,29 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createAppContainer } from "react-navigation";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import RDStatesScreen from './components/RDStates';
+import RDStateSitesScreen from './components/RDStateSites';
+import RDSiteGaugesScreen from './components/RDSiteGauges';
+import RDGaugeGraphScreen from './components/RDGaugeGraph';
+
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="States" component={RDStatesScreen} />
+        <Stack.Screen name="State Sites" component={RDStateSitesScreen} />
+        <Stack.Screen name="Site Gauges" component={RDSiteGaugesScreen} />
+        <Stack.Screen name="Gauge Graph" component={RDGaugeGraphScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
